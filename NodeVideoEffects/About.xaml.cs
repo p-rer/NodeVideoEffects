3f46604118bb617e5128a8eca2ab9815a14cc65c
rer <fodosoaodosoaoso@gmail.com>
2024-07-19 20:41:25 +0900

    add about window

---
+++ NodeVideoEffects/About.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NodeVideoEffects
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About(string git)
+        {
+            InitializeComponent();
+            this.git.Content = $"Git Commit ID: {git}";
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- NodeVideoEffects/MainWindow.xaml.cs
+++ NodeVideoEffects/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace NodeVideoEffects
 {
@@ -16,14 +19,30 @@ namespace NodeVideoEffects
     /// </summary>
     public partial class NodeEditor : Window
     {
+        string commitid;
         public NodeEditor()
         {
             InitializeComponent();
+            var asm = Assembly.GetExecutingAssembly();
+            var resName = asm.GetManifestResourceNames().FirstOrDefault(a => a.EndsWith("git_id.txt"));
+            if (resName == null)
+                commitid = string.Empty;
+            using (var st = asm.GetManifestResourceStream(resName))
+            {
+                if (st == null) commitid =  string.Empty;
+                var reader = new StreamReader(st);
+                commitid =  reader.ReadToEnd().Trim('\r', '\n');
+            }
         }
 
         public void AddChildren(Object obj, double x, double y)
         {
             EditSpace.AddChildren(obj, x, y);
+        }
+
+        private void ShowAbout(object sender, RoutedEventArgs e)
+        {
+            new About(commitid).ShowDialog();
         }
     }
 }
