4ce3d99de6303327a1c8df3aff1151e997030cb2
rer <fodosoaodosoaoso@gmail.com>
2024-07-24 13:39:08 +0900

    changed namespace and class name

---
+++ NodeVideoEffects.Type/INode.cs
@@ -0,0 +1,27 @@
+﻿namespace NodeVideoEffects.Type
+{
+    public abstract class INode
+    {
+        private Input[]? _inputs;
+        private Output[]? _outputs;
+        private string _name;
+
+        public Input[]? Inputs { get => _inputs; }
+        public Output[]? Outputs { get => _outputs; }
+        public string Name { get => _name; }
+
+        public INode(Input[]? inputs, Output[]? outputs, string name)
+        {
+            _inputs = inputs;
+            _outputs = outputs;
+            _name = name;
+        }
+
+        public void SetInput(int index, Object value)
+        {
+            _inputs[index].Value = value;
+        }
+
+        public abstract Task Calculate();
+    }
+}
