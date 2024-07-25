18607bb7b8c579110b27e5685e2cf0161c20db53
rer <fodosoaodosoaoso@gmail.com>
2024-07-25 13:22:46 +0900

    add sub node

---
+++ NodeVideoEffects.Nodes/Sub.cs
@@ -0,0 +1,24 @@
+﻿using NodeVideoEffects.Type;
+
+namespace NodeVideoEffects.Nodes
+{
+    public class Sub : INode
+    {
+        public Sub() : base(
+            [
+                new(new Number(0, null, null, null), "Value1"),
+                new(new Number(0, null, null, null), "Value2")
+            ],
+            [
+                new (new Number(0, null, null, null), "Result")
+            ],
+            "Sub")
+        { }
+
+        public override async Task Calculate()
+        {
+            this.Outputs[0].Value = (double)Inputs[0].Value - (double)Inputs[1].Value;
+            return;
+        }
+    }
+}
--- NodeVideoEffects.Test/NodeTest.cs
+++ NodeVideoEffects.Test/NodeTest.cs
@@ -12,10 +12,27 @@ namespace NodeVideoEffects.Test
             await AddNode.Calculate();
             Assert.AreEqual((double)AddNode.Outputs[0].Value, 0);
 
-            AddNode.Inputs[0].Value = 10.2;
-            AddNode.Inputs[1].Value = -4.54;
+            AddNode.SetInput(0, 10.2);
+            AddNode.SetInput(1, -4.54);
             await AddNode.Calculate();
             Assert.AreEqual((double)AddNode.Outputs[0].Value, 5.66);
+        }
+    }
+
+    [TestClass]
+    public class SubTest
+    {
+        Sub SubNode = new();
+        [TestMethod]
+        public async Task SubAsync()
+        {
+            await SubNode.Calculate();
+            Assert.AreEqual((double)SubNode.Outputs[0].Value, 0);
+
+            SubNode.SetInput(0, 2.633);
+            SubNode.SetInput(1, 4.3);
+            await SubNode.Calculate();
+            Assert.AreEqual((double)SubNode.Outputs[0].Value, -1.667);
         }
     }
 }
