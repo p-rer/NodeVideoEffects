4093efd8faa25d3c62a17c7ea2693da1eca6da50
p-rer <fodosoaodosoaoso@gmail.com>
2025-05-04 17:39:53 +0900

    Refactor LogItem and LogViewer to use dependency properties for better data binding and update handling

--- NodeVideoEffects.Utility/Logger.cs
+++ NodeVideoEffects.Utility/Logger.cs
@@ -4,27 +4,86 @@ namespace NodeVideoEffects.Utility;
 
 public static class Logger
 {
-    private static readonly LinkedList<(DateTime, LogLevel, string, object?)> Logs = [];
+    private static readonly LinkedList<Tuple<DateTime, LogLevel, string, object?>> Logs = [];
+    private static readonly Queue<Tuple<DateTime, LogLevel, string, object?>> ConsoleLogs = [];
+    private static byte _levels = 0x0F;
+    private static bool _isConsoleWriting;
+    
+    static Logger()
+    {
+        LogUpdated += (_, _) =>
+        {
+            if (_isConsoleWriting || ConsoleLogs.Count <= 0) return;
+            _isConsoleWriting = true;
+            WriteLogToConsole();
+            _isConsoleWriting = false;
+        };
+    }
+
+    private static void WriteLogToConsole()
+    {
+        while (true)
+        {
+            var log = ConsoleLogs.Dequeue();
+            Console.Write($@"{log.Item1:HH:mm:ss.fff}");
+            Console.ForegroundColor = log.Item2 switch
+            {
+                LogLevel.Debug => ConsoleColor.Green,
+                LogLevel.Info => ConsoleColor.Blue,
+                LogLevel.Warn => ConsoleColor.Yellow,
+                LogLevel.Error => ConsoleColor.Red,
+                _ => Console.ForegroundColor
+            };
+            Console.Write(@" {0,-5}", log.Item2);
+            Console.ResetColor();
+            Console.WriteLine(@"  {0,-20}", log.Item3);
+            if (log.Item4 != null)
+            {
+                Console.WriteLine(log.Item4);
+            }
+            Console.WriteLine();
+            if (ConsoleLogs.Count > 0) continue;
+            break;
+        }
+    }
 
     public static void Write(LogLevel level, string message, object? obj = null)
     {
-        Logs.AddLast((DateTime.Now, level, message, obj));
+        var log = new Tuple<DateTime, LogLevel, string, object?>(DateTime.Now, level, message, obj);
+        Logs.AddLast(log);
+        ConsoleLogs.Enqueue(log);
         if (Logs.Count > 1500) Logs.RemoveFirst();
-        LogUpdated.Invoke(null, EventArgs.Empty);
+        LogUpdated?.Invoke(null, EventArgs.Empty);
     }
-
-    public static ImmutableList<(DateTime, LogLevel, string, object?)> Read()
+    
+    public static void Clear()
     {
-        return Logs.ToImmutableList();
+        Logs.Clear();
+        LogUpdated?.Invoke(null, EventArgs.Empty);
     }
 
-    public static EventHandler LogUpdated = delegate { };
+    // levels: 0x01 = Debug, 0x02 = Info, 0x04 = Warn, 0x08 = Error
+    // levels can be combined, e.g. 0x03 = Debug + Info
+    public static void Filter(byte levels)
+    {
+        _levels = levels;
+        LogUpdated?.Invoke(null, EventArgs.Empty);
+    }
+
+    public static ImmutableList<Tuple<DateTime, LogLevel, string, object?>> Read()
+    {
+        return Logs
+            .Where(log => ((byte)log.Item2 & _levels) != 0)
+            .ToImmutableList();
+    }
+
+    public static EventHandler? LogUpdated;
 }
 
 public enum LogLevel
 {
-    Debug,
-    Info,
-    Warn,
-    Error
+    Debug = 0x01,
+    Info = 0x02,
+    Warn = 0x04,
+    Error = 0x08
 }
